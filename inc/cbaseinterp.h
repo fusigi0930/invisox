1c868b2dffc22cc78994f30f3b3ab63a6a8345ab
ChuYuan Chiang <chuyuan.chiang@gmail.com>
2014-09-10 15:56:49 +0800

    * creating the base interpreter class * adding the thread process functions   (multi thread support)   (maybe can use the QFutureWatcher to finish it!)

--- inc/cbaseinterp.h
+++ inc/cbaseinterp.h
@@ -4,17 +4,76 @@
 #include "debug.h"
 #include <QString>
 #include <QObject>
+#include <QMap>
+#include <QtConcurrent/QtConcurrent>
+#include <QFuture>
 
 class CBasedInterpreter : public QObject {
 	Q_OBJECT
 
 public:
-	CBasedInterpreter() {
-
+	CBasedInterpreter() : QObject(NULL), m_status(_STATUS_NORMAL) {
+	
 	}
 
 	virtual ~CBasedInterpreter() {
 
+	}
+
+	enum EInterpStatus {
+		_STATUS_NORMAL,
+		_STATUS_RUNNING,
+		_STATUS_TERMINATED,
+		_STATUS_STOP,
+		_STATUS_UNKNOW
+	};
+
+protected:
+	EInterpStatus m_status;
+	typedef QMap<QString, QFuture<int> > TInterpThreadMap;
+	TInterpThreadMap m_mapThread;
+
+	static int runThread(CBasedInterpreter *interp, QString szFile) {
+		if (NULL == interp || szFile.isEmpty()) {
+			return -1;
+		}
+
+		interp->run(szFile);
+		return interp->finishRun(szFile);		
+	}
+
+	virtual int run(QString szFile) {
+		return 0;
+	}
+
+	virtual int finishRun(QString szFile) {
+		emit sigThreadFinished(szFile);
+		// remove the thread from map
+		m_mapThread.remove(szFile);
+		return 0;
+	}
+
+signals:
+	void sigThreadFinished(QString szFile);
+	void sigThreadError(QString szFile);
+
+	
+public slots:
+	virtual int slotRun(QString szFile) {
+		if (szFile.isEmpty() {
+			return -1;
+		}
+
+		// is the file already running?
+		TInterpThreadMap::iterator pFind=m_mapThread.find(szFile);
+		if (m_mapThread.end() != pFind) {
+			return -2;
+		}
+		QFuture<int> thread=QtConcurrent::run(
+				CBasedInterpreter::runThread, this, szFile);
+
+		m_mapThread[szFile]=thread;
+		return 0;
 	}
 
 };
